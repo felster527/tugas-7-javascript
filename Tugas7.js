343b6b37c0201f02da0984b8cfff8b12527172ac
felster527 <91239878+felster527@users.noreply.github.com>
2023-08-21 16:18:46 +0700

    Add files via upload

---
+++ Tugas7.js
@@ -0,0 +1,12 @@
+var tinggiBadan = [160,165,163,166,167,172,163,168,173,178]
+
+for (var i = 0; i < tinggiBadan.length; i++){
+    console.log("tinggi badan - ",tinggiBadan[i])
+    
+}
+
+console.log("")
+
+for(let i of tinggiBadan){
+    console.log("tinggi badan - ",i)
+}
